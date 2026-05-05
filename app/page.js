3bbeca560200643468f3834209c3dbabff75b6f6
shanekid72 <business@lulufin.com>
2026-05-05 10:00:37 +0400

    Show only Demo1; disable video splash and other demos in rotation

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { getDailyDemo } from "@/lib/dailyDemo";
-import VideoSplash from "./VideoSplash";
+// import VideoSplash from "./VideoSplash";
 
 // Disable static optimization so the rotation re-evaluates on each request
 // (otherwise the picked demo would be frozen at build time).
@@ -7,9 +7,11 @@ export const dynamic = "force-dynamic";
 
 export default function Page() {
   const Demo = getDailyDemo();
-  return (
-    <VideoSplash>
-      <Demo />
-    </VideoSplash>
-  );
+  return <Demo />;
+  // Re-enable intro video splash by wrapping:
+  // return (
+  //   <VideoSplash>
+  //     <Demo />
+  //   </VideoSplash>
+  // );
 }

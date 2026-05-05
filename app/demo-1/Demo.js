3fc295fd060b10deecf5846e57e62346da7196b6
shanekid72 <business@lulufin.com>
2026-05-05 10:46:38 +0400

    demo-1: enlarge header logo to 190px desktop / 128px mobile

--- app/demo-1/Demo.js
+++ app/demo-1/Demo.js
@@ -30,7 +30,7 @@ const Demo1 = () => {
   return (
     <main className={`${facultyGlyphic.className} main-content-1 min-h-screen flex flex-col relative overflow-hidden`}>
       <header className="relative z-20 flex justify-center pt-8 sm:pt-10">
-        <img src="/logo-light.png" alt="Lulu Money Business" className="h-20 sm:h-28 w-auto opacity-95" />
+        <img src="/logo-light.png" alt="Lulu Money Business" className="h-32 sm:h-[190px] w-auto opacity-95" />
       </header>
 
       <div
